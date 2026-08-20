b7b04cadbc67175bdb6db35990348917b2457694
Shashwat Tiwari <shashwat132004@gmail.com>
2026-08-21 00:05:34 +0530

    16: Authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 import { TRPCReactProvider } from "@/trpc/client";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "next-themes";
@@ -26,6 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <TRPCReactProvider>
     <html lang="en" suppressHydrationWarning>
       <body
@@ -43,5 +45,6 @@ export default function RootLayout({
       </body>
     </html>
     </TRPCReactProvider>
+    </ClerkProvider>
   );
 }
--- modules/projects/server/procedures.ts
+++ modules/projects/server/procedures.ts
@@ -1,5 +1,5 @@
 import { prisma } from "@/lib/db";
-import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import z from "zod";
 import { inngest } from "../../../inngest/client";
 import { generateSlug } from "random-word-slugs";
@@ -7,16 +7,17 @@ import { TRPCError } from "@trpc/server";
 
 
 export const projectsRouter = createTRPCRouter({
-    getOne : baseProcedure
+    getOne : protectedProcedure
     .input(
         z.object({
             id: z.string().min(1,{message:"Project ID is required"}),
         }),
     )
-    .query(async({input})=>{
+    .query(async({input,ctx})=>{
         const existingProject = await prisma.project.findUnique({
             where: {
                 id: input.id,
+                userId: ctx.auth.userId,
             }, 
         });
         if(!existingProject){
@@ -27,16 +28,19 @@ export const projectsRouter = createTRPCRouter({
         }
         return existingProject;
     }),
-    getMany: baseProcedure
-    .query(async()=>{
+    getMany: protectedProcedure
+    .query(async({ctx})=>{
         const projects = await prisma.project.findMany({
+            where: {
+                userId: ctx.auth.userId,
+            },
             orderBy: {
                 updatedAt: "desc",
             }, 
         });
         return projects;
     }),
-    create: baseProcedure
+    create: protectedProcedure
     .input(
         z.object({
             value: z.string()
@@ -44,9 +48,10 @@ export const projectsRouter = createTRPCRouter({
             .max(10000,{message:"Prompt is too long"})
         }),
     )
-    .mutation(async ({input}) => {
+    .mutation(async ({input, ctx}) => {
         const createdProject=await prisma.project.create({
             data:{
+                userId: ctx.auth.userId,
                 name: generateSlug(3,{
                     format: "kebab",
                 }),
