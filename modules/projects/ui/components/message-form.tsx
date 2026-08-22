4ab0781483fb25befdcd59ab505a0135e484478a
Shashwat Tiwari <shashwat132004@gmail.com>
2026-08-22 21:45:16 +0530

    17: Billing

--- modules/projects/ui/components/message-form.tsx
+++ modules/projects/ui/components/message-form.tsx
@@ -10,6 +10,9 @@ import { Button } from "@/components/ui/button";
 import { Form , FormField} from "@/components/ui/form";
 import TextareaAutoSize from "react-textarea-autosize";
 import { ArrowUpIcon, Loader2Icon } from "lucide-react";
+import { Usage } from "@/modules/projects/ui/components/usage";
+import { useRouter } from "next/navigation";
+
 
 interface Props{
     projectId: string;  
@@ -23,6 +26,8 @@ const formSchema = z.object({
 export const MessageForm = ({projectId}:Props)=>{
     const trpc = useTRPC();
     const queryClient = useQueryClient();
+    const router=useRouter();
+    const {data:usage}=useQuery(trpc.usage.status.queryOptions());
      const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -36,9 +41,15 @@ export const MessageForm = ({projectId}:Props)=>{
             queryClient.invalidateQueries(
                 trpc.messages.getMany.queryOptions({projectId}),
             )
+            queryClient.invalidateQueries(
+                trpc.usage.status.queryOptions(),
+            )
         },
         onError: (error)=>{
             toast.error(error.message);
+            if(error.data?.code==="TOO_MANY_REQUESTS"){
+                router.push("/pricing");
+            }
         }
      }));
 
@@ -49,11 +60,17 @@ export const MessageForm = ({projectId}:Props)=>{
         })
      };
      const [isFocused, setIsFocused] = useState(false);
-     const showUsage = false;
+     const showUsage = !!usage;
      const isPending = createMessage.isPending;
      const isButtonDisabled = isPending || !form.formState.isValid;
     return(
         <Form {...form}>
+            {showUsage && (
+                <Usage
+                points={usage.remainingPoints}
+                msBeforeNext={usage.msBeforeNext}
+                />
+            )}
             <form onSubmit={form.handleSubmit(onSubmit)}
             className={
                 cn(
