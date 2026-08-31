2741f83f908cb23cb721799aa027e94598885d36
Shashwat Tiwari <shashwat132004@gmail.com>
2026-08-31 20:56:24 +0530

    19: bug-fixes

---
+++ app/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+const ErrorPage=()=>{
+    return(
+        <div>
+            GLOBAL ERROR!
+        </div>
+    )
+};
+export default ErrorPage;
--- modules/projects/ui/views/project-view.tsx
+++ modules/projects/ui/views/project-view.tsx
@@ -10,20 +10,26 @@ import { Suspense, useState } from "react";
 import { Fragment } from "@prisma/client";
 import { ProjectHeader } from "../components/project-header";
 import { FragmentWeb } from "../components/fragment-web";
-import { Tabs,TabsContent,TabsList,TabsTrigger } from "../../../../components/ui/tabs";
-import { EyeIcon,CodeIcon, CrownIcon} from "lucide-react";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "../../../../components/ui/tabs";
+import { EyeIcon, CodeIcon, CrownIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { FileExplorer } from "@/components/file-explorer";
 import { UserControl } from "@/components/user-control";
 import { useAuth } from "@clerk/nextjs";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface Props {
   projectId: string;
 }
 export const ProjectView = ({ projectId }: Props) => {
   const { has } = useAuth();
-  const hasProAccess=has?.({plan: "pro"});
+  const hasProAccess = has?.({ plan: "pro" });
   const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
   const [TabState, setTabState] = useState<"preview" | "code">("preview");
   return (
@@ -34,50 +40,57 @@ export const ProjectView = ({ projectId }: Props) => {
           minSize={20}
           className="flex flex-col min-h-0"
         >
-          <Suspense fallback={<p>Loading project...</p>}>
-            <ProjectHeader projectId={projectId} />
-          </Suspense>
-        <Suspense fallback={<p>Loading messages...</p>}>
-        <MessagesContainer 
-        projectId={projectId}
-        activeFragment={activeFragment}
-        setActiveFragment={setActiveFragment}
-        />
-        </Suspense>
+          <ErrorBoundary fallback={<p>Error loading project!</p>}>
+            <Suspense fallback={<p>Loading project...</p>}>
+              <ProjectHeader projectId={projectId} />
+            </Suspense>
+          </ErrorBoundary>
+          <ErrorBoundary fallback={<p>Error loading messages!</p>}>
+            <Suspense fallback={<p>Loading messages...</p>}>
+              <MessagesContainer
+                projectId={projectId}
+                activeFragment={activeFragment}
+                setActiveFragment={setActiveFragment}
+              />
+            </Suspense>
+          </ErrorBoundary>
         </ResizablePanel>
         <ResizableHandle className="hover:bg-primary transition-colors" />
         <ResizablePanel defaultSize={65} minSize={50}>
           <Tabs
-          className="h-full gap-y-0"
-          defaultValue="preview"
-          value={TabState}
-          onValueChange={(value)=>setTabState(value as "preview" | "code")}>
+            className="h-full gap-y-0"
+            defaultValue="preview"
+            value={TabState}
+            onValueChange={(value) => setTabState(value as "preview" | "code")}
+          >
             <div className="w-full flex items-center p-2 border-b gap-x-2">
               <TabsList className="h-8 p-0 border rounded-md">
                 <TabsTrigger value="preview" className="rounded-md">
-                  <EyeIcon/> <span>Demo</span>
+                  <EyeIcon /> <span>Demo</span>
                 </TabsTrigger>
                 <TabsTrigger value="code" className="rounded-md">
-                  <CodeIcon/> <span>Code</span>
+                  <CodeIcon /> <span>Code</span>
                 </TabsTrigger>
               </TabsList>
               <div className="ml-auto flex items-center gap-x-2">
                 {!hasProAccess && (
                   <Button asChild size="sm" variant="tertiary">
-                  <Link href="/pricing">
-                    <CrownIcon/> Upgrade
-                  </Link>
-                </Button>)}
-                <UserControl/>
+                    <Link href="/pricing">
+                      <CrownIcon /> Upgrade
+                    </Link>
+                  </Button>
+                )}
+                <UserControl />
               </div>
             </div>
-            <TabsContent value="preview" >
-              {!!activeFragment && <FragmentWeb data={activeFragment}/>}
+            <TabsContent value="preview">
+              {!!activeFragment && <FragmentWeb data={activeFragment} />}
             </TabsContent>
             <TabsContent value="code" className="min-h-0">
               {!!activeFragment?.files && (
                 <FileExplorer
-                files={activeFragment.files as {[path:string]:string}}/>
+                  files={activeFragment.files as { [path: string]: string }}
+                />
               )}
             </TabsContent>
           </Tabs>
